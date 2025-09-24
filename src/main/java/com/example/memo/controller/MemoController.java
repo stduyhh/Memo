122201586708d30209ec8f803da4358e834977f2
stduyhh <ghls1252@gmail.com>
2025-09-24 22:04:36 +0900

    memo api create

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -1,4 +1,64 @@
 package com.example.memo.controller;
 
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+//memo 생성
+@RestController
+@RequestMapping("/memos")
 public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+
+    @PostMapping
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto){
+        // 식별자 1씩 증가
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        //Request 데이터 객체 생성
+        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+
+        //메모 저장
+        memoList.put(memoId, memo);
+
+        return  new MemoResponseDto(memo);
+    }
+
+    //메모 조회
+    @GetMapping("/{id}")
+    public MemoResponseDto findMemoById(@PathVariable Long id){
+
+        Memo memo = memoList.get(id);
+
+        return new MemoResponseDto(memo);
+    }
+
+    //메모 수정
+    @PutMapping("/{id}")
+    public MemoResponseDto updateMemoById(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ){
+        Memo memo = memoList.get(id);
+
+        memo.update(dto);
+
+        return new MemoResponseDto(memo);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteMemo(@PathVariable Long id){
+        memoList.remove(id);
+    }
+
+
 }
--- src/main/java/com/example/memo/entity/Memo.java
+++ src/main/java/com/example/memo/entity/Memo.java
@@ -1,4 +1,21 @@
 package com.example.memo.entity;
 
+
+import com.example.memo.dto.MemoRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Memo {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+    public void update(MemoRequestDto dto){
+        this.title = dto.getTitle();
+        this.contents = dto.getContents();
+    }
+
 }
